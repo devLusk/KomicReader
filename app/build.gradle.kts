3a1ae6655a991cf9e17a3be54725af297b2a6224
DonizeteVida <donizetevida1998@outlook.com>
2025-10-07 00:46:40 +0200

    Add adaptative NavigationSuiteScaffold

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
 
     implementation(platform(libs.koin.bom))
     implementation(libs.bundles.koin)
--- app/src/main/java/com/dv/apps/komic/reader/MainActivity.kt
+++ app/src/main/java/com/dv/apps/komic/reader/MainActivity.kt
@@ -4,20 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationRail
-import androidx.compose.material3.NavigationRailItem
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.rememberNavigationSuiteScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
@@ -42,40 +36,41 @@ enum class Destination {
 
 @Composable
 fun MainScreen() {
-    var selectedDestination by remember { mutableIntStateOf(Destination.SONGS.ordinal) }
-    val backStack = remember { mutableStateListOf<Any>(Destination.SONGS) }
+    val navigationStack = remember { mutableStateListOf(Destination.SONGS) }
+    val navigationSuiteState = rememberNavigationSuiteScaffoldState()
 
-    Scaffold { contentPadding ->
-        Row {
-            NavigationRail(
-                Modifier.padding(contentPadding)
-            ) {
-                Destination.entries.forEachIndexed { index, destination ->
-                    NavigationRailItem(
-                        selected = selectedDestination == index,
-                        onClick = {
-                            backStack.add(destination)
-                            selectedDestination = index
-                        },
-                        icon = { Icon(painterResource(R.drawable.ic_android), contentDescription = null) },
-                        label = { Text(destination.name) }
-                    )
-                }
+    NavigationSuiteScaffold(
+        state = navigationSuiteState,
+        navigationSuiteItems = {
+            Destination.entries.forEachIndexed { index, destination ->
+                item(
+                    selected = navigationStack.first().ordinal == index,
+                    onClick = {
+                        navigationStack.clear()
+                        navigationStack.add(destination)
+                    },
+                    icon = {
+                        Icon(
+                            painterResource(R.drawable.ic_android),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text(destination.name) }
+                )
             }
-
-            NavDisplay(
-                modifier = Modifier.padding(contentPadding),
-                backStack = backStack,
-                onBack = { backStack.removeLastOrNull() },
-                entryProvider = entryProvider {
-                    entry<Destination> {
-                        when (it) {
-                            Destination.SONGS -> Text("Songs screen")
-                            Destination.FOLDER -> Text("Folder screen")
-                        }
+        },
+        containerColor = MaterialTheme.colorScheme.secondaryContainer
+    ) {
+        NavDisplay(
+            backStack = navigationStack,
+            entryProvider = entryProvider {
+                entry<Destination> {
+                    when (it) {
+                        Destination.SONGS -> Text("Songs screen")
+                        Destination.FOLDER -> Text("Folder screen")
                     }
                 }
-            )
-        }
+            }
+        )
     }
 }
