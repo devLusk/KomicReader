e3f0abec29df43794fbd2d495fd49cf70d867ee4
DonizeteVida <donizetevida1998@outlook.com>
2025-10-08 20:15:47 +0200

    Use Destination resources

--- app/src/main/java/com/dv/apps/komic/reader/feature/navigation/MainNavigation.kt
+++ app/src/main/java/com/dv/apps/komic/reader/feature/navigation/MainNavigation.kt
@@ -1,9 +1,10 @@
 package com.dv.apps.komic.reader.feature.navigation
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.material3.adaptive.navigationsuite.rememberNavigationSuiteScaffoldState
@@ -14,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation3.runtime.entryProvider
@@ -26,23 +28,26 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun MainNavigation() {
-    val navigationStack = remember { mutableStateListOf(Destination.SONGS) }
+    val navigationStack = remember { mutableStateListOf(Destination.HOME) }
     val navigationSuiteState = rememberNavigationSuiteScaffoldState()
 
     NavigationSuiteScaffold(
         state = navigationSuiteState,
         navigationSuiteItems = {
-            Destination.entries.forEachIndexed { index, destination ->
+            Destination.entries.forEach { destination ->
                 item(
-                    selected = navigationStack.first().ordinal == index,
-                    onClick = {
-                        navigationStack.clear()
-                        navigationStack.add(destination)
-                    },
+                    selected = navigationStack.single() == destination,
+                    onClick = { navigationStack.fill(destination) },
                     icon = {
                         Icon(
-                            painterResource(R.drawable.ic_android),
-                            contentDescription = null
+                            painterResource(
+                                if (navigationStack.single() == destination) {
+                                    destination.selectedIcon
+                                } else {
+                                    destination.icon
+                                }
+                            ),
+                            contentDescription = stringResource(destination.title)
                         )
                     },
                     label = { Text(destination.name) }
@@ -52,9 +57,9 @@ fun MainNavigation() {
         containerColor = MaterialTheme.colorScheme.secondaryContainer
     ) {
         if (LocalInspectionMode.current) {
-            Scaffold {
-                Text("Hello World", Modifier.padding(it))
-            }
+            Text("Hello World", Modifier.windowInsetsTopHeight(
+                WindowInsets.systemBars
+            ))
         } else {
             MainNavigator(navigationStack)
         }
@@ -70,8 +75,9 @@ fun MainNavigator(
         entryProvider = entryProvider {
             entry<Destination> {
                 when (it) {
-                    Destination.SONGS -> Text("Songs screen")
-                    Destination.FOLDER -> {
+                    Destination.HOME -> Text("Home")
+                    Destination.SHELF -> Text("Shelf")
+                    Destination.SETTINGS -> {
                         val vm = koinViewModel<FolderManagementViewModel>()
                         val state by vm.state.collectAsStateWithLifecycle()
                         FolderManagementScreen(state, vm::handleIntent)
@@ -82,9 +88,14 @@ fun MainNavigator(
     )
 }
 
-enum class Destination {
-    SONGS,
-    FOLDER
+enum class Destination(
+    val title: Int,
+    val icon: Int,
+    val selectedIcon: Int
+) {
+    HOME(R.string.menu_home, R.drawable.ic_home, R.drawable.ic_home_filled),
+    SHELF(R.string.menu_shelf, R.drawable.ic_shelf, R.drawable.ic_shelf_filled),
+    SETTINGS(R.string.menu_settings, R.drawable.ic_settings, R.drawable.ic_settings_filled)
 }
 
 @PreviewScreenSizes
